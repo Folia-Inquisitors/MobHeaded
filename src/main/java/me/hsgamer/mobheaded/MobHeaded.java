052196bec613af07903eb6303f29c4e5f0761d34
hsgamer <huynhqtienvtag@gmail.com>
2024-05-29 11:37:33 +0700

    init

---
+++ src/main/java/me/hsgamer/mobheaded/MobHeaded.java
@@ -0,0 +1,17 @@
+package me.hsgamer.mobheaded;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MobHeaded extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
